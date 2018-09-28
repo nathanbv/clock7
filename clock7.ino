9c9c0f0278d0c2d035d3ecffff0a535e235619eb
Nathounet <nathan.bleuzen@gmail.com>
2018-09-28 20:41:38 +0200

    Add TODO list

--- clock7.ino
+++ clock7.ino
@@ -1,3 +1,15 @@
+/*
+ * Nathan Bleuzen, 2018
+ *
+ * TODO list:
+ * - Monitor time drifting on long period between sync
+ * - 3D print 7seg display and assemble with LEDs
+ * - Add ws2812b 7seg digit implementation
+ * - Add display on 7seg of weather informations (numbers and colors/brightness)
+ * - Add REST request on weather station
+ * - Build weather station...
+ * - Add color/brightness shifting depending on time of the day
+ */
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <TimeLib.h>
@@ -5,7 +17,7 @@
 
 const char* wifiSSID = "ssid";
 const char* wifiPassword = "password";
-const char* restRequest = "http://api.timezonedb.com/v2.1/get-time-zone?key=API_KEY&format=json&by=zone&zone=Europe/Paris&fields=timestamp";
+const char* getTimeRequest = "http://api.timezonedb.com/v2.1/get-time-zone?key=API_KEY&format=json&by=zone&zone=Europe/Paris&fields=timestamp";
 const size_t jsonBufferSize = JSON_OBJECT_SIZE(3) + 50;
 time_t prevTime = 0; // when the last digital clock was displayed
 
@@ -28,7 +40,7 @@ void loop()
 {
     if (timeStatus() != timeNotSet)
     {
-        if (now() != prevTime) //update the display only if time has changed
+        if (now() != prevTime) // Update the display only if time has changed
         {
             prevTime = now();
             digital_clock_display();
@@ -69,7 +81,7 @@ void setup_wifi(void)
 time_t get_local_time(void)
 {
     HTTPClient http;
-    http.begin(restRequest);
+    http.begin(getTimeRequest);
     const int err = http.GET();
     if (err < 0)
     {
@@ -87,7 +99,7 @@ time_t get_local_time(void)
     if (strcmp(status, "OK") == 0)
     {
         unsigned long timestamp = root["timestamp"].as<unsigned long>();
-        long diff = prevTime - timestamp;
+        const long diff = prevTime - timestamp;
         Serial.print("Time sync: clock is ");
         Serial.print((diff < 0) ? -1*diff : diff);
         Serial.println((diff < 0) ? "sec late" : "sec ahead");
@@ -108,7 +120,7 @@ time_t get_local_time(void)
 /*********************/
 void digital_clock_display(void)
 {
-    // digital clock display of the time
+    // Digital clock display
     print_double_digit(hour());
     Serial.print(":");
     print_double_digit(minute());
@@ -125,7 +137,7 @@ void digital_clock_display(void)
 
 void print_double_digit(const int digits)
 {
-    // prints leading 0
+    // Prints leading 0
     if (digits < 10)
         Serial.print('0');
     Serial.print(digits);
